1658dd71f6a4c89d742367bb88bf8e193527000d
Bleidorb aka Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2019-10-10 12:44:29 +0200

    Add files via upload
    HomePage

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<?php
+  echo "Welcome in my domain >:)<br>";
+  echo '<a href="http://testdemophpweb.xp3.biz/H001/h001_1.php">H001/opdracht1</a>';
+?>
+
+  </body>
+</html>
