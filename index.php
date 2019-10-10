711f7c35b9f4321c45bbc33c0ae8e41ec26b4593
Bleidorb aka Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2019-10-10 14:19:38 +0200

    Update index.php
    homepage updated (added H002 links)

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
 
 <?php
   echo "Welcome in my domain >:)<br>";
-  echo '<a href="http://testdemophpweb.xp3.biz/H001/h001_1.php">H001/opdracht1</a>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H001/h001_1.php">H001/opdracht1</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H002/h002_1.php">H002/opdracht1</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H002/h002_2.php">H002/opdracht2</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H002/h002_3.php">H002/opdracht3</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H002/h002_4.php">H002/opdracht4</a><br>';
 ?>
 
   </body>
