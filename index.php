9e2fb76ea44b12c3bb9dd4f27b659fd04abf7346
Bleidorb aka Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2019-10-13 21:54:33 +0200

    Update index.php
    Added new links (and new title)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>HomePage //Opdrachten & Hoofdstukken//</title>
   </head>
   <body>
 
@@ -13,6 +13,14 @@
   echo '<a href="http://testdemophpweb.xp3.biz/H002/h002_2.php">H002/opdracht2</a><br>';
   echo '<a href="http://testdemophpweb.xp3.biz/H002/h002_3.php">H002/opdracht3</a><br>';
   echo '<a href="http://testdemophpweb.xp3.biz/H002/h002_4.php">H002/opdracht4</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_1.php">H003/opdracht1</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_2.php">H003/opdracht2</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_3.php">H003/opdracht3</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_4.php">H003/opdracht4</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_5.php">H003/opdracht5</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_6.php">H003/opdracht6</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_7.php">H003/opdracht7</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H004/h004_1.php">H004/opdracht1</a><br>';
 ?>
 
   </body>
