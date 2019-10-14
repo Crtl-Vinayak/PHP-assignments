30d6c0260eb3ebd5acc10440dbb3f3d36eff2570
Bleidorb aka Vinayak <39634679+VinvinVinayakvinVin@users.noreply.github.com>
2019-10-14 23:49:06 +0200

    Update index.php
    added new links

--- index.php
+++ index.php
@@ -21,6 +21,12 @@
   echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_6.php">H003/opdracht6</a><br>';
   echo '<a href="http://testdemophpweb.xp3.biz/H003/h003_7.php">H003/opdracht7</a><br>';
   echo '<a href="http://testdemophpweb.xp3.biz/H004/h004_1.php">H004/opdracht1</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H005/form.html">H005/form</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H005/form2.html">H005/form2</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H005/beasties_form.html">H005/beasties_form</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H005/beasties_form2.html">H005/beasties_form2</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H005/loginform.html">H005/loginform</a><br>';
+  echo '<a href="http://testdemophpweb.xp3.biz/H005/loginform2.html">H005/loginform2</a><br>';
 ?>
 
   </body>
